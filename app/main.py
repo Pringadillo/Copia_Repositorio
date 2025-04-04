b381a9e38a29e8e23e2f71beb8c5b863978e27ed
Karlets-i-mes <karltotimes@gmail.com>
2025-04-04 20:37:16 +0000

    incorporar librerias

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ import pandas as pd
 import sqlite3
 import os
 import pathlib
-
+import json
+import tempfile
 
 import appbar
 import cuerpo
 
 def main(page: ft.Page):
-    page.title = "Mi Aplicación Flet"
+    page.title = "Cuentas de Casa"
+    #page.icon = "/imagenes/"
     page.appbar = appbar.crear_appbar(page)
     page.add(cuerpo.crear_cuerpo())
 
