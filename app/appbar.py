72ed288a0f402c631dbbfc383d93c5c5faeac77d
Karlets-i-mes <karltotimes@gmail.com>
2025-04-08 12:25:10 +0000

    más botones

--- app/appbar.py
+++ app/appbar.py
@@ -18,53 +18,90 @@ def crear_appbar(page):
     appbar = ft.AppBar(
         title=ft.Row(
             [
-                ft.Container(
-                    content=ft.TextButton(
-                        text="Diario",
-                        on_click=boton_click,
-                        style=ft.ButtonStyle(
-                            text_style=ft.TextStyle(size=18, letter_spacing=2)
+                # Primeros botones centrados
+                ft.Row(
+                    [
+                        ft.TextButton(
+                            text="Tabla Códigos",
+                            icon=ft.icons.ACCOUNT_TREE,  # Ícono para el botón "Tabla Código"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
                         ),
-                    ),
-                    margin=ft.margin.only(right=50),  # Ajusta la distancia desde la derecha
+                        ft.TextButton(
+                            text="Diario",
+                            icon=ft.icons.BOOK,  # Ícono para el botón "Diario"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
+                        ),
+                        ft.TextButton(
+                            text="Mayor",
+                            icon=ft.icons.LIST,  # Ícono para el botón "Mayor"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
+                        ),
+                        ft.TextButton(
+                            text="Perdidas y Ganancias",
+                            icon=ft.icons.PIE_CHART,  # Ícono para el botón "Perdidas y Ganancias"
+                            #icon=ft.icons.BALANCE,  # Ícono para el botón "Perdidas y Ganancias"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
+                        ),                        
+                        ft.TextButton(
+                            text="Balance",
+                            icon=ft.icons.BALANCE,  # Ícono para el botón "Balance"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
+                        ),
+
+                        ft.TextButton(
+                            text="sumas y Saldos",
+                            icon=ft.icons.ATTACH_MONEY,  # Ícono para el botón "Sunas y Saldos"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
+                        ),
+                        ft.TextButton(
+                            text="Inversiones",
+                            icon=ft.icons.ATTACH_MONEY,  # Ícono para el botón "Inversiones"
+                            on_click=boton_click,
+                            style=ft.ButtonStyle(
+                                text_style=ft.TextStyle(size=18, letter_spacing=2)
+                            ),
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,  # Centrar los primeros botones
+                    expand=True,  # Ocupa el espacio disponible
                 ),
+                # Último botón a la derecha
                 ft.TextButton(
-                    text="Mayor",
+                    text="Configuración",
+                    icon=ft.icons.SETTINGS,  # Ícono para el botón "Configuración"
                     on_click=boton_click,
                     style=ft.ButtonStyle(
                         text_style=ft.TextStyle(size=18, letter_spacing=2)
                     ),
                 ),
                 ft.TextButton(
-                    text="Balance",
-                    on_click=boton_click,
-                    style=ft.ButtonStyle(
-                        text_style=ft.TextStyle(size=18, letter_spacing=2)
-                    ),
-                ),
-                ft.TextButton(
-                    text="Saldos",
-                    on_click=boton_click,
-                    style=ft.ButtonStyle(
-                        text_style=ft.TextStyle(size=18, letter_spacing=2)
-                    ),
-                ),
-                ft.TextButton(
-                    text="Tabla Códigos",
+                    text="Salir",
+                    icon=ft.icons.EXIT_TO_APP_OUTLINED,  # Ícono para el botón "Salir"
                     on_click=boton_click,
                     style=ft.ButtonStyle(
                         text_style=ft.TextStyle(size=18, letter_spacing=2)
                     ),
                 ),                
-                ft.TextButton(
-                    text="Configuración",
-                    on_click=boton_click,
-                    style=ft.ButtonStyle(
-                        text_style=ft.TextStyle(size=18, letter_spacing=2)
-                    ),
-                ),
             ],
-            alignment=ft.MainAxisAlignment.CENTER,  # Centrado horizontal
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,  # Espaciado entre los grupos
         )
     )
     return appbar
