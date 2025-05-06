70f14ae5fbcaed7839b4c5aa7b85e2d5072aee4a
Pringadillo <karleskode@gmail.com>
2025-05-06 08:49:01 +0000

    anular la correcion de ahora mismo

--- data/funciones_BD.py
+++ data/funciones_BD.py
@@ -85,9 +85,7 @@ def ver_tabla_nivel1():
 
     # Formatea los resultados como una lista de cadenas
     if resultados:
-        print("Datos de la tabla nivel1:")
-        for fila in resultados:
-            print(f"{fila[0]} - {fila[1]}")
+        return [f"{fila[0]} - {fila[1]}" for fila in resultados]
     else:
         return ["La tabla nivel1 está vacía."]
 
