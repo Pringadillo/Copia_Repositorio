0074f21a9b8a443129441391e6206a2d4f440cc3
Pringadillo <karleskode@gmail.com>
2025-05-13 05:26:35 +0000

    Mover data e imagenes

--- app/cuerpo.py
+++ app/cuerpo.py
@@ -15,12 +15,16 @@ def crear_cuerpo(page, contenido):
     )
 
 
+#https://www.freepik.es/foto-gratis/casa-aislada-campo_10025444.htm
+#"https://via.placeholder.com/150"
+
+
 contenido_por_defecto = ft.Column(
     [
         ft.Text("Bienvenido a la aplicación de Cuentas de Casa", size=20, weight=ft.FontWeight.BOLD),
         ft.Text("Aquí puedes gestionar tus cuentas y gastos de manera eficiente.", size=16),
         ft.Container(
-            content=ft.Image(src="https://via.placeholder.com/150", width=150, height=150),
+            content=ft.Image(src="https://www.freepik.es/foto-gratis/casa-aislada-campo_10025444.htm", width=150, height=150),
             border=ft.border.all(2, ft.colors.BLACK)
         ),
         ft.Text("Fecha actual: " + str(datetime.datetime.now().date()), size=16)
